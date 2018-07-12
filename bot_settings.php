7464f349129b6e2972c88035a678303e78f0f6ab
Klongchan App <cukjsystem@gmail.com>
2018-07-12 19:59:50 +0700

    text

---
+++ bot_settings.php
@@ -0,0 +1,7 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1593599546');
+define('LINE_MESSAGE_CHANNEL_SECRET','4d11691cb2e8d4ebbb079210d61008b3');
+define('LINE_MESSAGE_ACCESS_TOKEN','02PYTYiuEYCnDi7O40r2ncI28D21QlbLh1/3MqMPGRFGsxza+QHFL6NjG3BLP2JnINDSmmpmpNScV7rwPeiEohbMwuaQ4xgze4j1aaJmfS6WB0ULhtx+Gr3D5qGicdrwsQ7hG98utVrbtt/DRhAIVwdB04t89/1O/w1cDnyilFU=');
+?>
+
